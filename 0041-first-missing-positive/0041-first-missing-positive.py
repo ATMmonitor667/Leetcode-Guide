55c4e954843e29a1e43149e38acd1a4978ccf5a1
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2025-10-24 22:10:50 -0400

    Time: 50 ms (70.55%), Space: 26.1 MB (19.03%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        mini = min(nums)
+        maxi = max(nums)
+        if mini > 1:
+            return 1
+        if maxi <= 0:
+            return 1
+        nums = set(nums)
+        miniPositive = 1
+        if miniPositive not in nums:
+            return miniPositive
+        
+        while True:
+            if miniPositive + 1 in nums:
+                miniPositive = miniPositive + 1
+            if miniPositive + 1 not in nums:
+                nums = []
+                return miniPositive + 1
