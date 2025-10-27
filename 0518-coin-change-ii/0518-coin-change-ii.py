d412ac35374a436f8a15bd467e7fcc622ba02702
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2025-10-27 03:26:09 -0400

    Time: 3632 ms (5.45%), Space: 180.3 MB (5.22%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -11,8 +11,6 @@ class Solution(object):
                 return 1
             if amount < 0:
                 return 0
-            if index > len(coins):
-                return 
             if (index, amount) in memo:
                 return memo[(index, amount)]
             else:
