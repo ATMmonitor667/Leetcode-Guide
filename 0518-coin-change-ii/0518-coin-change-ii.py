eb6afeb4f78799bd6c3a50d42994ca9b5a957612
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2025-10-27 03:25:50 -0400

    Time: 3986 ms (5.11%), Space: 180.3 MB (5.22%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        memo = {}
+        def dfs(index, amount):
+            if amount == 0:
+                return 1
+            if amount < 0:
+                return 0
+            if index > len(coins):
+                return 
+            if (index, amount) in memo:
+                return memo[(index, amount)]
+            else:
+                res = 0
+                if index < len(coins):
+                   res = dfs(index, amount -coins[index]) + dfs(index+1, amount)
+                memo[(index, amount)] = res
+                return res
+        val = dfs(0, amount)
+        return val
+        
