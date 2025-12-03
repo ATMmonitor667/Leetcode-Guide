c394aeff0e413484e0aac9ca729802140fb03e1c
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2025-12-03 15:18:06 -0500

    Time: 374 ms (5.2%), Space: 15.5 MB (8.26%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        memo = {}
+        def dfs(index, target):
+            if target == 0:
+                return 1 
+            if target < 0:
+                return 0
+            if index > len(nums)-1:
+                return 0
+            if (index, target) in memo:
+                return memo[(index, target)]
+            else:
+                res = 0
+                for i in range(0, len(nums)):
+                    res += dfs(i, target - nums[i])
+                memo[(index, target)] = res
+                return res
+        
+        return dfs(0, target)
