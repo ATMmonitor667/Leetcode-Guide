9554c1376b4c153543806d33962272f9ef4d1365
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-01-11 21:02:45 -0500

    Time: 243 ms (35.94%), Space: 27.9 MB (66.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,54 +1,23 @@
-class DSU:
-    def __init__(self, n):
-        self.parent = [i for i in range(n)]
-        self.size = [1 for _ in range(n)]
-
-    def find(self, x):
-        if x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, a, b):
-        A = self.find(a)
-        B = self.find(b)
-
-        if A == B:
-            return False
-
-        if self.size[A] < self.size[B]:
-            A, B = B, A
-
-        self.parent[B] = A
-        self.size[A] += self.size[B]
-        return True
-
-
 class Solution(object):
     def numIslands(self, grid):
-        if not grid or not grid[0]:
-            return 0
+        m = len(grid)-1
+        n = len(grid[0])-1
+        def dfs(r,c):
+            if not(0 <= r<=m) or not(0<=c<=n) or grid[r][c] == '0':
+                return 
+            else:
+                grid[r][c] = '0'
+                dfs(r+1,c)
+                dfs(r-1,c)
+                dfs(r,c+1)
+                dfs(r,c-1)
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i,j)
+                    count+=1
+        return count
 
-        rows, cols = len(grid), len(grid[0])
-        dsu = DSU(rows * cols)
-
-        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-        def idx(r, c):
-            return r * cols + c   
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1':
-                    for dr, dc in dirs:
-                        nr, nc = r + dr, c + dc
-                        if 0 <= nr < rows and 0 <= nc < cols:
-                            if grid[nr][nc] == '1':
-                                dsu.union(idx(r, c), idx(nr, nc))
-
-        roots = set()
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1':
-                    roots.add(dsu.find(idx(r, c)))
-
-        return len(roots)
+            
+     
