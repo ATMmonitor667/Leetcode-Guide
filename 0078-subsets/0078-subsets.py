d1f9b68f39a954ac59e7906bf331a94afcad5944
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-01-11 21:09:51 -0500

    Time: 0 ms (100%), Space: 12.7 MB (50.77%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,32 +3,17 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
-        index = 0,1,2,3,4, 5
-        nums = [4,5,6,23,45] []
-        [1,2,3]
-        [], index = 0
-        nums[index] = [1]
-        path = [] or [1], index = 0
-        nums[index] = [2] index = 1
-        add [2] or not [2]
-        path = [] or [2] or [1] or [1, 2] 
-        index = 2
-        num[index] = 3
-        add [3] or not [3]
-        path = [] or [3] or [2] or [2,3]  or [1] or [1,3] or [1, 2] or [1,2,3]
-        index == 3
-        res = []
-
         """
         res = []
-        def dfs(index, path):
-            if index > len(nums):
-                return 
-            if index == len(nums):
+        def dfs(i, path):
+            if i == len(nums):
                 res.append(path[:])
                 return 
             else:
-                dfs(index+1, path+[nums[index]])
-                dfs(index+1, path)
-        dfs(0, [])
+                dfs(i+1,path + [nums[i]])
+                dfs(i+1,path)
+        dfs(0,[])
         return res
+
+
+        
