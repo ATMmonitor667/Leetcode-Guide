05faf7b72f61e3ea3b926717a5c3aac3a4ffd97c
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-01-18 16:12:16 -0500

    Time: 0 ms (100%), Space: 12.5 MB (76.75%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,21 +10,19 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        li3 = ListNode(0)
-        cursor = li3
+        dummy = ListNode(0)
+        cursor = dummy
+
         while list1 and list2:
             if list1.val <= list2.val:
-                cursor.next = ListNode(list1.val)
-                cursor = cursor.next
+                cursor.next = list1
                 list1 = list1.next
-            elif list1.val > list2.val:
-                cursor.next = ListNode(list2.val)
-                cursor = cursor.next
+            else:
+                cursor.next = list2
                 list2 = list2.next
-        if list1:
-            cursor.next = list1
-        else:
-            cursor.next = list2
-        return li3.next
-            
+            cursor = cursor.next
+
+        cursor.next = list1 if list1 else list2
+        return dummy.next
+
 
