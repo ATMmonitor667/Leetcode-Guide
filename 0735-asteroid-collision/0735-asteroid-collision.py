e1005fd0adec4aa00fda0a15c5923f16a9746d3f
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-01-18 19:49:36 -0500

    Time: 15 ms (17.11%), Space: 13.4 MB (6.93%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        stack = []
+
+        for a in asteroids:
+            alive = True
+
+            while alive and stack and stack[-1] > 0 and a < 0:
+                if abs(stack[-1]) < abs(a):
+                    stack.pop()         
+                    continue
+                elif abs(stack[-1]) == abs(a):
+                    stack.pop()         
+                    alive = False
+                else:
+                    alive = False       
+
+            if alive:
+                stack.append(a)
+
+        return stack
+
+  
