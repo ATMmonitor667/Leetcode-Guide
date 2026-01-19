f285771357f5e18ddd6325d016aa89a8adb68958
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-01-19 03:22:10 -0500

    Time: 17 ms (87.34%), Space: 16.1 MB (70.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,13 +5,8 @@ class Solution(object):
         :rtype: List[List[str]]
         What is the best way to do this?
         """
-        groups = defaultdict(list)
-
+        graph = defaultdict(list)
         for s in strs:
-            count = [0] * 26
-            for ch in s:
-                count[ord(ch) - ord('a')] += 1
-            groups[tuple(count)].append(s)
-
-        return list(groups.values())
-    
+            key = ''.join(sorted(s))
+            graph[key].append(s)
+        return [val for val in graph.values()]
