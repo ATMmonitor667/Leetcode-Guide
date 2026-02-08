268c39305c997460a0106800b3abc036d764cd7e
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-02-08 14:57:56 -0500

    Time: 1 ms (77.15%), Space: 14.7 MB (51.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,17 @@
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        auto function = [](vector<int> &v, int target) -> vector<int>
-            {
-            unordered_map<int, int>mp;
-            for(size_t i = 0; i < v.size(); i++)
-            {
-                int complement = target - v[i];
-                if(mp.find(complement) != mp.end())
-                {
-                return {mp[complement], static_cast<int>(i)};
-                }
-                mp[v[i]] = i;
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.contains(complement)) {
+                return {i, mp[complement]};
             }
-            return {-1,-1};
-            };
-      return function(nums, target);
-                    
+
+            mp[nums[i]] = i;
+        }
+        return {};
     }
 };
