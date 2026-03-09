79500e5e9e6e97a8c88e58886e61705e7fb72ab0
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-03-09 17:42:30 -0400

    Time: 1915 ms (5.04%), Space: 45.8 MB (5.91%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,15 +1,42 @@
 class Solution(object):
+    class UnionFind():
+        def __init__(self, vals):
+            self.parent = {v: v for v in vals}
+            self.rank   = {v: 1 for v in vals}
+        def find(self, x):
+            if x != self.parent[x]:
+                self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
+        def union(self, a, b):
+            rootA = self.find(a)
+            rootB = self.find(b)
+            if rootA == rootB:
+                return
+            else:
+                if a not in self.parent or b not in self.parent:
+                    return
+                if self.rank[rootB] > self.rank[rootA]:
+                    rootA, rootB = rootB, rootA
+                self.parent[rootB] = rootA
+                self.rank[rootA]+= self.rank[rootB]
+                return True
+        def returnMaxRank(self):
+            maxi = 0
+            for k,v in self.rank.items():
+                if maxi < v:
+                    maxi = v
+            print(self.rank)
+            return maxi
     def longestConsecutive(self, nums):
-        num_set = set(nums)
-        ans = 0
-        
-        for num in num_set:
-            if num - 1 not in num_set:
-                sum = 1
-                atual = num + 1
-                while atual in num_set:
-                    sum += 1
-                    atual += 1
-                ans = max(ans, sum)
-
-        return ans
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums: return 0
+        nums = list(set(nums))          # remove dups
+        DSU = self.UnionFind(nums)
+        numset = set(nums)
+        for x in nums:
+            if x - 1 in numset:
+                DSU.union(x, x - 1)
+        return DSU.returnMaxRank()
