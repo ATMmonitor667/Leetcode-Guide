e0dcb1705888f69b9249e3fb9b60ee84927414b0
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-06-02 18:33:44 -0400

    Time: 4 ms (7.46%), Space: 15.7 MB (7.54%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-
-void dfs(
-        int index,
-        vector<int>& nums,
-        vector<int>& path,
-        vector<vector<int>>& result
-    ) {
-        if (index == nums.size()) {
-            result.push_back(path);
-            return;
-        }
-
-        path.push_back(nums[index]);
-        dfs(index + 1, nums, path, result);
-        path.pop_back();
-        dfs(index + 1, nums, path, result);
-    }
-
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
+
+        auto func = [&](auto&& func,
+                         vector<vector<int>>& result,
+                         vector<int>& nums,
+                         int index,
+                         vector<int> path) -> void {
+
+            if (index == nums.size()) {
+                result.push_back(path);
+                return;
+            }
+
+            path.push_back(nums[index]);
+            func(func, result, nums, index + 1, path);
+
+            path.pop_back();
+            func(func, result, nums, index + 1, path);
+        };
+
         vector<int> path;
-        dfs(0, nums, path, result);
+        func(func, result, nums, 0, path);
+
         return result;
     }
 };
