5d6ba8fc4a9111f57694db26fa8db261c1e36989
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-06-10 00:51:19 -0400

    Time: 3 ms (27.54%), Space: 16.3 MB (5.11%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,28 +1,70 @@
 class Solution {
 public:
-    void dfs(
-        vector<int>& nums,
-        int index, 
-        vector<int>& path,
-        vector<vector<int>>&result
-    )
-    {
-        if(index == nums.size())
-        {
-            result.push_back(path);
-            return;
+    template <typename T>
+    class SuperVector {
+    private:
+        std::vector<T> data;
+
+    public:
+        SuperVector() = default;
+
+        SuperVector(const std::vector<T>& vec)
+            : data(vec) {}
+
+        SuperVector(std::initializer_list<T> vals)
+            : data(vals) {}
+
+        void append(const T& value) {
+            data.push_back(value);
         }
-        else{
-            path.push_back(nums[index]);
-            dfs(nums, index+1, path, result);
-            path.pop_back();
-            dfs(nums, index+1, path, result);
+
+        void pop() {
+            if (!data.empty())
+                data.pop_back();
         }
-    }
+
+        SuperVector& operator+=(const SuperVector& other) {
+            for (const auto& v : other.data) {
+                data.push_back(v);
+            }
+            return *this;
+        }
+
+        SuperVector operator+(const SuperVector& other) const {
+            SuperVector result = *this;
+            result += other;
+            return result;
+        }
+
+        size_t size() const {
+            return data.size();
+        }
+
+        std::vector<T> toVector() const {
+            return data;
+        }
+    };
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int>path;
-        dfs(nums, 0, path, result);
-        return result;
+        SuperVector<SuperVector<int>> result;
+
+        auto dfs = [&](auto&& dfs, int index, SuperVector<int> path) -> void {
+            if (index == nums.size()) {
+                result.append(path);
+                return;}
+
+            dfs(dfs,index + 1,path + SuperVector<int>{nums[index]});
+            dfs(dfs,index + 1,path);
+        };
+
+        dfs(dfs, 0, SuperVector<int>{});
+
+        vector<vector<int>> answer;
+
+        for (auto subset : result.toVector()) {
+            answer.push_back(subset.toVector());
+        }
+
+        return answer;
     }
 };
