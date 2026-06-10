05b5febeae52fec10483b2ff9f9338ae627a7949
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-06-10 01:04:07 -0400

    Time: 6 ms (3.37%), Space: 15.7 MB (10.75%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,67 +1,29 @@
+
+vector<int> operator+(vector<int> lhs, int x)
+{
+    lhs.push_back(x);
+    return lhs;
+}
+
 class Solution {
 public:
-    template <typename T>
-    class SuperVector {
-    private:
-        std::vector<T> data;
-
-    public:
-        SuperVector() = default;
-
-        SuperVector(const std::vector<T>& vec)
-            : data(vec) {}
-
-        SuperVector(std::initializer_list<T> vals)
-            : data(vals) {}
-
-        void append(const T& value) {
-            data.push_back(value);
-        }
-
-        void pop() {
-            if (!data.empty())
-                data.pop_back();
-        }
-
-        SuperVector& operator+=(const SuperVector& other) {
-            for (const auto& v : other.data) {
-                data.push_back(v);
-            }
-            return *this;
-        }
-
-        SuperVector operator+(const SuperVector& other) const {
-            SuperVector result = *this;
-            result += other;
-            return result;
-        }
-
-        size_t size() const {
-            return data.size();
-        }
-
-        std::vector<T> getData() const {
-            return data;
-        }
-    };
-
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
 
-        auto dfs = [&](auto&& dfs, int index, SuperVector<int> path) -> void {
-            if (index == nums.size()) {
-                auto value = path.getData();
-                result.push_back(value);
-                return;}
+        auto dfs = [&](auto&& dfs,
+                       int index,
+                       vector<int> path) -> void {
 
-            dfs(dfs,index + 1,path + SuperVector<int>{nums[index]});
-            dfs(dfs,index + 1,path);
+            if (index == nums.size()) {
+                result.push_back(path);
+                return;
+            }
+
+            dfs(dfs, index + 1, path + nums[index]);
+            dfs(dfs, index + 1, path);
         };
 
-        dfs(dfs, 0, SuperVector<int>{});
-
-        
-      
+        dfs(dfs, 0, vector<int>{});
 
         return result;
     }
