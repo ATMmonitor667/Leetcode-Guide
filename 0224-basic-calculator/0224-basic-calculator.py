b74c6e46a319a19f51f8b122cbb4b9267cf4074d
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-07-03 17:49:05 -0400

    Time: 471 ms (5.03%), Space: 27.3 MB (5.03%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,39 +1,69 @@
+class Parser:
+    def __init__(self):
+        self.index = 0
+        self.value = 0
+        self.s = ""
+
+    def parse(self, s):
+        self.s = s
+        self.index = 0
+        return self.expr()
+
+    def _skip_spaces(self):
+        while self.index < len(self.s) and self.s[self.index] == " ":
+            self.index += 1
+
+    def _apply(self, op, a, b):
+        if op == "+":
+            return a + b
+        if op == "-":
+            return a - b
+        if op == "*":
+            return a * b
+        if op == "/":
+            return a // b
+        return 0
+
+    def number(self):
+        self._skip_spaces()
+        self.value = 0
+        while self.index < len(self.s) and self.s[self.index].isdigit():
+            self.value = self.value * 10 + int(self.s[self.index])
+            self.index += 1
+        return self.value
+
+    def factor(self):
+        self._skip_spaces()
+        if self.index < len(self.s) and self.s[self.index] == "(":
+            self.index += 1
+            val = self.expr()
+            self._skip_spaces()
+            if self.index < len(self.s) and self.s[self.index] == ")":
+                self.index += 1 
+            return val
+        return self.number()
+
+    def term(self):
+        result = self.factor()
+        self._skip_spaces()
+        while self.index < len(self.s) and self.s[self.index] in "*/":
+            op = self.s[self.index]
+            self.index += 1
+            result = self._apply(op, result, self.factor())
+            self._skip_spaces()
+        return result
+
+    def expr(self):
+        result = self.term()
+        self._skip_spaces()
+        while self.index < len(self.s) and self.s[self.index] in "+-":
+            op = self.s[self.index]
+            self.index += 1
+            result = self._apply(op, result, self.term())
+            self._skip_spaces()
+        return result
+
 class Solution(object):
     def calculate(self, s):
-        stack = []
-        res = 0
-        number = 0
-        sign = 1
-
-        i = 0
-        while i < len(s):
-            ch = s[i]
-
-            if ch.isdigit():
-                number = number * 10 + int(ch)
-
-            elif ch == '+':
-                res += sign * number
-                number = 0
-                sign = 1
-
-            elif ch == '-':
-                res += sign * number
-                number = 0
-                sign = -1
-
-            elif ch == '(':
-                stack.append((res, sign))
-                res = 0
-                sign = 1
-                number = 0
-
-            elif ch == ')':
-                res += sign * number
-                number = 0
-                prev_res, prev_sign = stack.pop()
-                res = prev_res + prev_sign * res
-
-            i += 1
-
-        return res + sign * number
+        solver = Parser()
+        return solver.parse(s)
