db765d47ae200e487aa6801fe2726e423c77a485
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-07-08 17:44:41 -0400

    Time: 7 ms (33.28%), Space: 12.3 MB (91.23%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -4,4 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        return sorted(nums, key=lambda val: (Counter(nums)[val], -val))
+        has = {}
+        for num in nums:
+            has[num] = has.get(num, 0) + 1
+        nums.sort(key=lambda x: (has[x], -x))
+        return nums
