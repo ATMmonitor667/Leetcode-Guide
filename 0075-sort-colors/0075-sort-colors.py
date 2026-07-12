7f84d7738d89212387d285b1181ced92faa1225c
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-07-12 19:18:41 -0400

    Time: 0 ms (100%), Space: 12.3 MB (54.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,5 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
-        return nums
+        count = Counter(nums)
+        index = 0
+
+        for value in range(3):
+            for _ in range(count[value]):
+                nums[index] = value
+                index += 1
