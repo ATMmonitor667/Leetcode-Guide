e4c142dadd5d1ab81e9cdbfd88a14bd3e5658a4d
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-07-12 13:50:12 -0400

    Time: 10 ms (59.38%), Space: 14.1 MB (73.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,16 +5,8 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        myDict = {}
-        for x in nums:
-            myDict[x] = myDict.get(x, 0) + 1
-
-        sorted_items = sorted(myDict.items(), key=lambda x: x[1], reverse=True)
-
-        res = []
-        for i in range(min(k, len(sorted_items))):
-            res.append(sorted_items[i][0])
-        return res
+        return [x for x, _ in Counter(nums).most_common(k)]
+       
 
         
         
