adadceb06621897147544de91f309a5c332681d4
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-07-12 14:02:08 -0400

    Time: 43 ms (25.18%), Space: 14.3 MB (15.19%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -6,16 +6,4 @@ class Solution(object):
         :type x: int
         :rtype: List[int]
         """
-        heap = []
-        for i in arr:
-            heapq.heappush(heap, [abs(i-x),i])
-        ans = []
-        while heap and k:
-            val = heapq.heappop(heap)
-            ans.append(val[1])
-            k-=1
-        print(ans)
-        ans.sort()
-        return ans
-
-        
+        return sorted(sorted(arr, key=lambda i: (abs(i - x), i))[:k])
