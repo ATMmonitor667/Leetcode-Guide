03092f001fdbaaff46a770acb27c0350ba799959
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-07-12 23:28:52 -0400

    Time: 12 ms (47.83%), Space: 12.6 MB (58.59%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -6,14 +6,16 @@ class Solution(object):
         """
         a = -1
         b = -1
-        seen = set()
+        hashMap = {}
         n = len(grid)
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] in seen:
-                    a = grid[i][j]
-                seen.add(grid[i][j])
         for i in range(1, n*n+1):
-            if i not in seen:
-                b = i
+            hashMap[i] = 0
+        for i in range(n):
+            for j in range(n):
+                hashMap[grid[i][j]] = hashMap.get(grid[i][j], 0) +1
+        for key, value in hashMap.items():
+            if value == 0:
+                b = key
+            elif value == 2:
+                a = key
         return [a,b]
