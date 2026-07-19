57e8a1c69633658148df2588499299525ad03ff5
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-07-19 02:08:52 -0400

    Time: 1 ms (54.96%), Space: 10 MB (11.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-   bool isAnagram(string s, string t) {
-    if(s.size() != t.size())
-        return false;
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
 
-    unordered_map<char,int> freq;
+        unordered_map<char, int> freq;
 
-    for(char c : s)
-        freq[c]++;
+        for (char c : s) {
+            freq[c]++;
+        }
 
-    for(char c : t) {
-        if(--freq[c] == 0)
-            freq.erase(c);
+        for (char c : t) {
+            auto it = freq.find(c);
+
+            if (it == freq.end()) {
+                return false;
+            }
+
+            it->second--;
+
+            if (it->second == 0) {
+                freq.erase(it);
+            }
+        }
+
+        return freq.empty();
     }
-
-    return freq.empty();
-}
 };
