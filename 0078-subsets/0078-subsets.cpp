394637f25ddfa5e5fc91187d59e842a27ad5fd70
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-07-27 02:10:06 -0400

    Time: 3 ms (27.6%), Space: 15.7 MB (11.25%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,30 +1,29 @@
-
-vector<int> operator+(vector<int> lhs, int x)
-{
-    lhs.push_back(x);
-    return lhs;
-}
-
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
+    void dfs(
+        int index,
+        vector<int> path,
+        vector<vector<int>>& result,
+        vector<int>& nums
+    )
+    {
+        if (index == nums.size())
+        {
+            result.push_back(path);
+            return;
+        }
+
+        path.push_back(nums[index]);
+        dfs(index + 1, path, result, nums);
+
+        path.pop_back();
+        dfs(index + 1, path, result, nums);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums)
+    {
         vector<vector<int>> result;
-
-        auto dfs = [&](auto&& dfs,
-                       int index,
-                       vector<int> path) -> void {
-
-            if (index == nums.size()) {
-                result.push_back(path);
-                return;
-            }
-
-            dfs(dfs, index + 1, path + nums[index]);
-            dfs(dfs, index + 1, path);
-        };
-
-        dfs(dfs, 0, vector<int>{});
-
+        dfs(0, {}, result, nums);
         return result;
     }
 };
