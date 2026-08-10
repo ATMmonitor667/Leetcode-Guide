8d63e5113b0eb660918b0bed609574a363d5c537
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-08-10 16:12:07 -0400

    Time: 0 ms (100%), Space: 12.5 MB (45.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,20 +1,27 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        rot = bisect_left(nums, True, key=lambda n: n <= nums[-1])
-        
-        lo, hi = 0, n - 1
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
 
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            real = (mid + rot) % n
+        while left <= right:
+            mid = (left + right) // 2
 
-            if nums[real] == target:
-                return real
-                
-            if nums[real] < target:
-                lo = mid + 1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[left]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             else:
-                hi = mid - 1
-
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
         return -1
