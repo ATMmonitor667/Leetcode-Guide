d67bc14bd8b9dd1b9ee53e44777a5ae792d8227f
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-08-10 15:23:07 -0400

    Time: 201 ms (19.36%), Space: 17.7 MB (6.35%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,34 +1,40 @@
 class Solution(object):
     def minRemoveToMakeValid(self, s):
-        # PASS 1: Remove invalid ')'
-        stack = []
-        count = 0  # Tracks open parentheses
-        
-        for char in s:
-            if char == '(':
-                stack.append(char)
-                count += 1
-            elif char == ')':
-                if count > 0:
-                    stack.append(char)
-                    count -= 1
-                # Else: count is 0, so this ')' is invalid. Skip it (do not append).
-            else:
-                stack.append(char)
-        
-        # If count is 0, we are done!
-        if count == 0:
-            return "".join(stack)
-            
-        # PASS 2: Remove excess '(' from the end
-        # We iterate backwards through our temporary 'stack'
         ans = []
-        for char in reversed(stack):
-            if char == '(' and count > 0:
-                # This is an extra '('. Skip it.
+        count = 0
+        for i in range(len(s)):
+            if s[i] != "(" and s[i] != ")":
+                ans.append(s[i])
+
+            if s[i] == ")" and count == 0:
+                continue
+
+            if s[i] == "(":
+                ans.append("(")
+                count += 1
+
+            if s[i] == ")" and count > 0:
+                ans.append(")")
                 count -= 1
-            else:
-                ans.append(char)
-        
-        # Since we built 'ans' backwards, reverse it again to finish
-        return "".join(ans[::-1])
+
+        word = ''.join(ans)
+
+        removeStack = []
+
+        for index, char in enumerate(word):
+            if char == "(":
+                removeStack.append((char, index))
+            elif char == ")":
+                removeStack.pop()
+
+        removeIndex = set()
+
+        for char, index in removeStack:
+            removeIndex.add(index)
+
+        word = ''.join(
+            char for index, char in enumerate(word)
+            if index not in removeIndex
+        )
+
+        return word
