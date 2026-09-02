bb746e9f4e1ba5d999ca8f4b101e87f7078a51fc
A.T.M. Hossain <125620701+ATMmonitor667@users.noreply.github.com>
2026-09-01 21:30:37 -0400

    Time: 225 ms (59.67%), Space: 26.1 MB (99.97%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,29 +3,30 @@ class Solution(object):
         """
         :type grid: List[List[str]]
         :rtype: int
-        To solve this problem we need to implement dfs
-        we should only be able to move left right up and down so we need a dirs
-        array with all the directions, we then will need a visit array
-        which if we see it in the visit array we will not visit it again
-        The visit array is what is stopping the exponential explosion.
-
-        Outside the DFS function we will simply use a forloop.
         """
-        m = len(grid)-1
-        n = len(grid[0])-1
-        directions = [(-1,0),(1,0),(0,-1),(0,1)]
-        visit = set()
+        def prune(grid):
+            for i in range(len(grid)):
+                for j in range(len(grid[0])):
+                    if grid[i][j] == "1":
+                        grid[i][j] = 1
+                    else:
+                        grid[i][j] = 0
+            return grid
         def dfs(r,c):
-            if r < 0 or r > m or c < 0 or c > n or grid[r][c] == "0" or (r,c) in visit:
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 0:
                 return
-            visit.add((r,c))
-            for x,y in directions:
-                dfs(r+x, c+y)
+            else:
+                grid[r][c] = 0
+                dfs(r+1,c)
+                dfs(r-1,c)
+                dfs(r, c-1)
+                dfs(r, c+1)
+        grid = prune(grid)
         count = 0
-        
-        for i in range(m+1):
-            for j in range(n+1):
-                if grid[i][j] == "1" and (i,j) not in visit:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    count +=1
                     dfs(i,j)
-                    count += 1
-        return count
+        return count 
+        
